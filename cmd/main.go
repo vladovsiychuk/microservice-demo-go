38e34573755a8ec185a579462a4d4d453474c7bb
vladovsiychuk <vladyslav.ovsiychuk@gmail.com>
2024-11-01 21:51:07 +0200

    rename folder

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/vladovsiychuk/microservice-demo-go/configs"
-	backendtofrontend "github.com/vladovsiychuk/microservice-demo-go/internal/backend-to-frontend"
+	backendforfrontend "github.com/vladovsiychuk/microservice-demo-go/internal/backend-for-frontend"
 	"github.com/vladovsiychuk/microservice-demo-go/internal/comment"
 	"github.com/vladovsiychuk/microservice-demo-go/internal/post"
 	"github.com/vladovsiychuk/microservice-demo-go/internal/shared"
@@ -47,13 +47,13 @@ func injectDependencies(postgresDB *gorm.DB, mongoDB *mongo.Database, eventBus *
 	commentHandler := comment.NewRouter(commentService)
 	commentHandler.RegisterRoutes(r)
 
-	postAggregateRepository := backendtofrontend.NewPostAggregateRepository(mongoDB)
-	bffService := backendtofrontend.NewService(postAggregateRepository)
-	eventHandler := backendtofrontend.NewEventHandler(bffService)
+	postAggregateRepository := backendforfrontend.NewPostAggregateRepository(mongoDB)
+	bffService := backendforfrontend.NewService(postAggregateRepository)
+	eventHandler := backendforfrontend.NewEventHandler(bffService)
 	setupSubscribers(eventBus, eventHandler)
 }
 
-func setupSubscribers(eventBus *eventbus.EventBus, eventHandler *backendtofrontend.EventHandler) {
+func setupSubscribers(eventBus *eventbus.EventBus, eventHandler *backendforfrontend.EventHandler) {
 	postCreatedChan := make(chan eventbus.Event)
 	eventBus.Subscribe(shared.PostCreatedEventType, postCreatedChan)
 	go eventHandler.PostCreatedHandler(postCreatedChan)
