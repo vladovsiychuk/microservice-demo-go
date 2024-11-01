425f934a2e9ee90539f23910b69f3e2cdf942041
vladovsiychuk <vladyslav.ovsiychuk@gmail.com>
2024-11-01 11:45:36 +0200

    implement post aggregate

---
+++ internal/backend-to-frontend/post_aggregate.go
@@ -0,0 +1,30 @@
+package backendtofrontend
+
+import (
+	"github.com/google/uuid"
+	"github.com/vladovsiychuk/microservice-demo-go/internal/post"
+)
+
+type CommentItem struct {
+	Id      uuid.UUID
+	Content string
+}
+
+type PostAggregate struct {
+	Id        uuid.UUID
+	Content   string
+	IsPrivate bool
+	comments  []CommentItem
+}
+
+type PostAggregateI interface {
+}
+
+var CreatePostAggregate = func(post *post.Post) (PostAggregateI, error) {
+	return &PostAggregate{
+		post.Id,
+		post.Content,
+		post.IsPrivate,
+		[]CommentItem{},
+	}, nil
+}
