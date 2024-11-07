f4eaac3ee9763371ce42bd7bc6754e4526f7d350
vladovsiychuk <vladyslav.ovsiychuk@gmail.com>
2024-11-07 20:39:29 +0200

    implement create key logic

--- internal/auth/keys.go
+++ internal/auth/keys.go
@@ -1,10 +1,18 @@
 package auth
 
 type Keys struct {
-	privateKey         string
-	publicKey          string
-	secondaryPublicKey string
+	PrivateKey         string
+	PublicKey          string
+	SecondaryPublicKey string
 }
 
 type KeysI interface {
+}
+
+var CreateKeys = func(privateKey string, publicKey string, secondaryPublicKey string) (KeysI, error) {
+	return &Keys{
+		privateKey,
+		publicKey,
+		secondaryPublicKey,
+	}, nil
 }
