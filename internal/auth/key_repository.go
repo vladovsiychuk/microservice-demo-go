b6212bab15cded7f4da51a669413a8799a1b5353
vladovsiychuk <vladyslav.ovsiychuk@gmail.com>
2024-11-07 20:39:13 +0200

    add repository method

--- internal/auth/key_repository.go
+++ internal/auth/key_repository.go
@@ -6,6 +6,20 @@ type KeyRepository struct {
 	postgresDB *gorm.DB
 }
 
+type KeyRepositoryI interface{}
+
+func NewKeyRepository(postgresDB *gorm.DB) *KeyRepository {
+	return &KeyRepository{
+		postgresDB,
+	}
+}
+
 func (r *KeyRepository) Create(keys KeysI) error {
 	return r.postgresDB.Create(keys).Error
+}
+
+func (r *KeyRepository) GetKeys() (KeysI, error) {
+	var keys Keys
+	err := r.postgresDB.First(&keys).Error
+	return &keys, err
 }
